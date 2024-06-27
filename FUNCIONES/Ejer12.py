41779ad82065ca950bd55b235be728ac08843961
Daniel Aldana Lopez <230110150@itsoeh.edu.mx>
2024-06-27 09:21:32 -0600

    FINAL 12

--- FUNCIONES/Ejer12.py
+++ FUNCIONES/Ejer12.py
@@ -8,7 +8,7 @@ def calcular_estadisticas_intervalos(datos):
     parte_entera_num_clases = int(num_clases_calculado)
     parte_decimal_num_clases = num_clases_calculado - parte_entera_num_clases
 
-    if parte_decimal_num_clases < 0.5:
+    if (parte_decimal_num_clases < 0.5):
         num_clases_redondeado = parte_entera_num_clases
     else:
         num_clases_redondeado = parte_entera_num_clases + 1
@@ -28,7 +28,7 @@ def obtener_limites_y_marcas(datos, num_clases, ancho_clase):
     for i in range(int(num_clases)):
         limite_inferior = np.min(datos) + i * ancho_clase
         limite_superior = limite_inferior + ancho_clase
-        limite_medio = (limite_inferior + limite_superior) / 2  # Calcula el punto medio
+        limite_medio = (limite_inferior + limite_superior) / 2
         limites_clases.append((limite_inferior, limite_superior))
         limite_inferior_array.append(limite_inferior)
         limite_superior_array.append(limite_superior)
@@ -42,11 +42,23 @@ def calcular_frecuencias(datos, limite_inferior, limite_superior):
         frec_abs = np.sum((datos >= lim_inf) & (datos < lim_sup))
         frecuencia_absoluta.append(frec_abs)
 
-    total_datos = len(datos)
-    frecuencia_relativa = [frec / total_datos for frec in frecuencia_absoluta]
-    frecuencia_acumulada = np.cumsum(frecuencia_absoluta)
+    frecuencia_relativa = frec_rel(frecuencia_absoluta)
+    frecuencia_acumulada = frec_acum(frecuencia_relativa)
 
     return frecuencia_absoluta, frecuencia_relativa, frecuencia_acumulada
+
+def frec_rel(fa_absoluta):
+    total_datos = sum(fa_absoluta)
+    fr = [(frec / total_datos) * 100 for frec in fa_absoluta]
+    return fr
+
+def frec_acum(fr_relativa):
+    fr_acumulada = []
+    fr_ac = 0
+    for fr in fr_relativa:
+        fr_ac += fr
+        fr_acumulada.append(fr_ac)
+    return fr_acumulada
 
 def crear_tabla(datos):
     limite_inferior, limite_superior, marcas_clases = calcular_estadisticas_intervalos(datos)
@@ -64,24 +76,20 @@ def crear_tabla(datos):
 
     return tabla
 
-def plot_grafOjiva(marcas_clase, valores_ref_eje, facum):
+def plot_grafOjiva(marcas_clase, facum):
     """
     Función para dibujar una ojiva que muestra la frecuencia acumulada por marca de clase.
     
     Parámetros:
-    - marcas_clase (list): lista de strings que representan las marcas de clase
-    - valores_ref_eje (list): lista de números que representan los valores de referencia para cada marca de clase
+    - marcas_clase (list): lista de valores que representan las marcas de clase
     - facum (list): lista de números que representan la frecuencia acumulada para cada marca de clase
     """
     
-    # Crear listas para los datos de la ojiva
-    datos_x_ojiva = [0] + valores_ref_eje
+    datos_x_ojiva = list(range(len(marcas_clase) + 1))
     datos_y_ojiva = [0] + facum
 
-    # Crear una figura con un tamaño específico
     plt.figure(figsize=(15, 5))
     
-    # Dibujar la ojiva
     plt.plot(datos_x_ojiva, datos_y_ojiva, 
              linewidth=3, 
              color="#1C2833",
@@ -92,18 +100,14 @@ def plot_grafOjiva(marcas_clase, valores_ref_eje, facum):
              markeredgecolor="#EC0000",
              markeredgewidth=1.5) 
 
-    # Etiquetas para el eje x
-    plt.xticks(valores_ref_eje, marcas_clase, fontsize=10)
+    plt.xticks(datos_x_ojiva, [f"{marca:.2f}" for marca in [0] + marcas_clase], fontsize=10)
     
-    # Título y etiquetas para los ejes
     plt.xlabel("Marcas de clase", fontsize=15)
-    plt.ylabel("Frecuencia acumulada", fontsize=15)
+    plt.ylabel("Frecuencia acumulada (%)", fontsize=15)
     plt.title("Ojiva", fontsize=20)
     
-    # Agregar una grilla
     plt.grid()
     
-    # Mostrar el gráfico
     plt.show()
 
 def plot_grafHist(frecuencias_relativas, marcas_clase, marcas_texto):
@@ -112,38 +116,27 @@ def plot_grafHist(frecuencias_relativas, marcas_clase, marcas_texto):
     
     Parámetros:
     - frecuencias_relativas (list): lista de números que representan la frecuencia relativa de cada marca de clase
-    - marcas_clase (list): lista de strings que representan las marcas de clase
+    - marcas_clase (list): lista de valores que representan las marcas de clase
     - marcas_texto (list): lista de strings que representan los nombres de cada marca de clase
     """
     
-    # Crear una figura con un tamaño específico
     plt.figure(figsize=(12, 6))
     
-    # Crear un arreglo de indices para las columnas
     x = np.arange(len(marcas_clase))
-    
-    # Ancho de las columnas
     width = 0.5
     
-    # Dibujar las columnas del histograma
     plt.bar(x, frecuencias_relativas, width, 
              edgecolor="k", 
              color=["#FF5733", "#33FF57", "#5733FF", "#FF33A6", "#33FFF6"])
     
-    # Etiquetas para el eje x
     plt.xticks(x, marcas_texto, fontsize=15, rotation=45)
     
-    # Título y etiquetas para los ejes
     plt.xlabel("Marcas de clase", fontsize=20)
-    plt.ylabel("Frecuencia relativa", fontsize=20)
+    plt.ylabel("Frecuencia relativa (%)", fontsize=20)
     plt.title("Histograma", fontsize=25)
     
-    # Ajustar el límite superior del eje y
     plt.ylim(0, max(frecuencias_relativas) * 1.1)
     
-    # Agregar una grilla
     plt.grid()
     
-    # Mostrar el gráfico
     plt.show()
-
